879eb6cf87fc0feb336c67d73ebcd59587d74339
Izuho Fujiwara <fujishan2212@gmail.com>
2021-01-17 00:57:40 +0900

    test

--- .github/script/restart.sh
+++ .github/script/restart.sh
@@ -4,6 +4,6 @@ set -e
 ssh mountain-server <<SHELL
 
 echo "$1" | sudo -S systemctl stop tdu21-discord-bot
-echo "$1" | sudo -S systemctl start tdu21-discord-bot
+# echo "$1" | sudo -S systemctl start tdu21-discord-bot
 
 SHELL
