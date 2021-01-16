977163412cc5717340c584f11c9882948dc361ef
Izuho Fujiwara <fujishan2212@gmail.com>
2021-01-17 01:06:28 +0900

    Add: update script

---
+++ .github/script/update.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+BRANCH=main
+
+ssh mountain-server <<SHELL
+
+echo "$1" | sudo -S systemctl stop tdu21-discord-bot
+
+cd /home/tdu21/bot/tdu21-discord-bot
+
+git checkout "$BRANCH"
+git fetch origin
+git reset --hard origin/"$BRANCH"
+
+yarn install
+
+echo "$1" | sudo -S systemctl start tdu21-discord-bot
+
+SHELL
