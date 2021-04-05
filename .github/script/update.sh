8ef6923ed91dd72bbde4e3deccd879626136c6f6
Izuho Fujiwara <fujishan2212@gmail.com>
2021-04-05 11:37:46 +0900

    Add: typeorm & database

--- .github/script/update.sh
+++ .github/script/update.sh
@@ -17,13 +17,17 @@ yarn install
 
 rm -rf .env
 
-echo "DISCORD_BOT_TOKEN=$2" >> .env
-echo "MODERATOR_ROLE_ID=$3" >> .env
-echo "LOGDNA_TOKEN=$4" >> .env
-echo "LOGDNA_APP=$5" >> .env
-echo "SENDGRID_API_KEY=$6" >> .env
-echo "SENDGRID_FROM_EMAIL=$7" >> .env
-echo "DENDAI_EMAIL_DOMAIN=$8" >> .env
+echo "DISCORD_BOT_TOKEN="$2 >> .env
+echo "MODERATOR_ROLE_ID="$3 >> .env
+echo "LOGDNA_TOKEN="$4 >> .env
+echo "LOGDNA_APP="$5 >> .env
+echo "SENDGRID_API_KEY="$6 >> .env
+echo "SENDGRID_FROM_EMAIL="$7 >> .env
+echo "DENDAI_EMAIL_DOMAIN="$8 >> .env
+echo "DB_HOST="$9 >> .env
+echo "DB_USERNAME="${10} >> .env
+echo "DB_PASSWORD="${11} >> .env
+echo "DB_DATABASE="${12} >> .env
 
 echo "$1" | sudo -S systemctl start tdu21-discord-bot
 
--- index.ts
+++ index.ts
@@ -1,8 +1,12 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
+import "reflect-metadata";
+
 import { Client } from "@typeit/discord";
 import { logger } from "./utils/logger";
+import { connectDatabase } from "./utils/database";
+import { getConnection } from "typeorm";
 
 async function start() {
   const client = new Client({
@@ -12,12 +16,25 @@ async function start() {
     partials: ["MESSAGE", "CHANNEL", "REACTION"],
   });
 
+  console.info("Try login discord...");
   await client.login(process.env.DISCORD_BOT_TOKEN);
+
+  try {
+    console.info("Try connectoin database...");
+    await connectDatabase();
+  } catch (error) {
+    console.error("Database connectoin error...");
+    process.exit();
+  }
 
   logger.info("Welcome to TDU21-Discord Bot...");
 
-  process.on("SIGINT", () => {
+  process.on("SIGINT", async () => {
     logger.info("Shutdown TDU21-Discord Bot...");
+
+    await getConnection().close();
+    logger.info("Close database connectoin...");
+
     client.destroy();
   });
 }
