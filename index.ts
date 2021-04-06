bc4f3f1a4d4cf3c91549ae17df856b55a7781850
kazuemon <git@k6n.jp>
2021-04-06 15:53:30 +0900

    add: express の追加

--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 import { Client } from "@typeit/discord";
+import * as express from 'express'
+import { TextChannel } from "discord.js";
+const app: express.Express = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 async function start() {
   const client = new Client({
@@ -11,6 +16,19 @@ async function start() {
   });
 
   await client.login(process.env.DISCORD_BOT_TOKEN);
+
+  app.listen(3000, () => {
+    console.log("Webhookサーバー起動")
+  })
+
+  app.get('/webhook', async (req, res) => {
+    const guild = client.guilds.cache.find((guild) => guild.id === "796374774158065674")
+    console.log(guild)
+    const channel = guild.channels.cache.find((ch) => ch.id === "797070664988491807") as TextChannel
+    console.log(channel)
+    await channel.send("テスト")
+    res.end();
+  })
 
   process.on("SIGTERM", () => {
     console.log("session destroy...");
