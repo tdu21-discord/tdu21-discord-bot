1d920ddd37779b7a22f79704119c6e98a4779e4a
Izuho Fujiwara <fujishan2212@gmail.com>
2021-04-14 01:16:51 +0900

    Fix: config path

--- commands/scripts/RemoveEveryonesDepRole.ts
+++ commands/scripts/RemoveEveryonesDepRole.ts
@@ -1,6 +1,6 @@
 import { CommandMessage } from "@typeit/discord";
 import { Guild } from "discord.js";
-import departments from "../../config/departments";
+import serverConfig from "../../config";
 import { logger } from "../../utils/logger";
 
 const RemoveEveryonesDepRole = async (message: CommandMessage) => {
@@ -10,7 +10,7 @@ const RemoveEveryonesDepRole = async (message: CommandMessage) => {
   members.forEach((member, key) => {
     if (!member.user.bot){
       member.roles.cache.forEach((role) => {
-        departments.forEach((dep) => {
+        serverConfig.departments.forEach((dep) => {
           if (dep.departmentRoleId === role.id) {
             logger.debug("Removed: " + member.user.username + " / " + role.name)
             member.roles.remove(role)
