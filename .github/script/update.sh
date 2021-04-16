e464a6404efcc11ee50556c8255db372e2f14905
Izuho Fujiwara <fujishan2212@gmail.com>
2021-04-16 22:41:15 +0900

    Remove :env

--- .github/script/update.sh
+++ .github/script/update.sh
@@ -15,23 +15,6 @@ git reset --hard origin/"$BRANCH"
 
 yarn install
 
-rm -rf .env
-
-echo "ENV="$2 >> .env
-echo "DISCORD_BOT_TOKEN="$3 >> .env
-echo "MODERATOR_ROLE_ID="$4 >> .env
-echo "LOGDNA_TOKEN="$5 >> .env
-echo "LOGDNA_APP="$6 >> .env
-echo "SENDGRID_API_KEY="$7 >> .env
-echo "SENDGRID_FROM_EMAIL="$8 >> .env
-echo "SENDGRID_FROM_NAME="$9 >> .env
-echo "DENDAI_EMAIL_DOMAIN="${10} >> .env
-echo "DB_HOST="${11} >> .env
-echo "DB_USERNAME="${12} >> .env
-echo "DB_PASSWORD="${13} >> .env
-echo "DB_DATABASE="${14} >> .env
-echo "STUDENT_ID_VERIFY_MAX="${15} >> .env
-
 echo "$1" | sudo -S systemctl start tdu21-discord-bot
 
 SHELL
