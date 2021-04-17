855900491e77b24fcb006a0b3e3cda241012e635
kazuemon <git@k6n.jp>
2021-04-17 18:12:39 +0900

    change: 環境変数の反映方法を変更

--- .github/script/update.sh
+++ .github/script/update.sh
@@ -5,7 +5,7 @@ BRANCH=main
 
 ssh mountain-server <<SHELL
 
-echo "$1" | sudo -S systemctl stop tdu21-discord-bot
+echo "${PASSWORD}" | sudo -S systemctl stop tdu21-discord-bot
 
 cd /home/tdu21/bot/tdu21-discord-bot
 
@@ -15,6 +15,21 @@ git reset --hard origin/"$BRANCH"
 
 yarn install
 
-echo "$1" | sudo -S systemctl start tdu21-discord-bot
+echo "ENV=${ENV}" >> .env
+echo "DISCORD_BOT_TOKEN=${DISCORD_BOT_TOKEN}" >> .env
+echo "MODERATOR_ROLE_ID=${MODERATOR_ROLE_ID}" >> .env
+echo "LOGDNA_TOKEN=${LOGDNA_TOKEN}" >> .env
+echo "LOGDNA_APP=${LOGDNA_APP}" >> .env
+echo "SENDGRID_API_KEY=${SENDGRID_API_KEY}" >> .env
+echo "SENDGRID_FROM_EMAIL=${SENDGRID_FROM_EMAIL}" >> .env
+echo "SENDGRID_FROM_NAME=${SENDGRID_FROM_NAME}" >> .env
+echo "DENDAI_EMAIL_DOMAIN=${DENDAI_EMAIL_DOMAIN}"$ >> .env
+echo "DB_HOST=${DB_HOST}" >> .env
+echo "DB_USERNAME=${DB_USERNAME}" >> .env
+echo "DB_PASSWORD=${DB_PASSWORD}" >> .env
+echo "DB_DATABASE=${DB_DATABASE}" >> .env
+echo "STUDENT_ID_VERIFY_MAX=${STUDENT_ID_VERIFY_MAX}" >> .env
+
+echo "${PASSWORD}" | sudo -S systemctl start tdu21-discord-bot
 
 SHELL
